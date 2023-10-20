924e64fe6860e55f31be269fb048e440edd5d886
SBH05 <seobohye5@gmail.com>
2023-10-20 18:53:34 +0900

    수정

--- minipro/TeamHamster/src/main/java/com/teamproject/hamster/controller/CartController.java
+++ minipro/TeamHamster/src/main/java/com/teamproject/hamster/controller/CartController.java
@@ -2,15 +2,18 @@ package com.teamproject.hamster.controller;
 
 import com.teamproject.hamster.view.ListView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class CartController {
-    Scanner sc = new Scanner(System.in);
     ListView LM = new ListView();
+    Scanner sc = new Scanner(System.in);
 
+    public void shopBasket(ArrayList select) {
 
-    public void shopBasket() { //
-
+       // System.out.println(select);
 
         while (true) {
 
@@ -21,20 +24,28 @@ public class CartController {
             System.out.print("번호를 입력 하세요 : ");
             int num = sc.nextInt();
             switch (num) {
-                case 1: LM.ShoesSize(); break; // 선택한 상품 사이즈 변경 Key 값을 기입
-                case 2: cartCancel(); break;
-                case 3: LM.kindOfShoes(); break;
-                case 4:  break; //s
+                case 1:   break; // 선택한 상품 사이즈 변경 Key 값을 기입
+                case 2: //cartCancel(select); break;
+                case 3:  break;
+                case 4://  PaymentController.print(select);
+                        break; //select
             }
 
         }
     }
 
 
-    public void cartCancel () {  }
+    public void cartCancel(ArrayList cancelList) {
+
+      //  List<ProductDTO> cancelList = new ArrayList<>();
+
+    }
 
 
-    public void changeSize() {
+
+
+    public void changeSize() { //스위치
+
 
     }
 
